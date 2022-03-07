857147e3aec650ba555f2156c3859242eb3ff771
Jan Sterle <jan.sterle123@gmail.com>
2022-03-07 11:58:59 +0100

    novi grafi

--- components/admin/poizvedbe.php
+++ components/admin/poizvedbe.php
@@ -21,11 +21,68 @@ function numberOfZnamkaLastMonth($conn) {
             WHERE created_at > ADDDATE(CURDATE(), -30)
             GROUP BY znamka";
 
-if ($result = mysqli_query($conn,$sql)) {
-    return $result;
-} else {
-    return false;
+    if ($result = mysqli_query($conn,$sql)) {
+        return $result;
+    } else {
+        return false;
+    }
 }
+
+function numberOfModelLastMonth($conn) {
+    $sql = "SELECT `status`,znamka,model,COUNT(voziloID) as koliko,AVG(cena) as povprecna_cena
+            FROM Oglas
+            WHERE created_at > ADDDATE(CURDATE(), -30)
+            GROUP BY znamka,model";
+    
+    if ($result = mysqli_query($conn,$sql)) {
+        return $result;
+    } else {
+        return false;
+    }
+}
+
+
+function averageCenaAvtov($conn) {
+    $sql = "SELECT AVG(cena) as povprecna_cena FROM Oglas";
+
+    if ($result = mysqli_query($conn,$sql)) {
+        return $result;
+    } else {
+        return false;
+    }
+
+}
+
+function averageCenaNaZnamko($conn) {
+    $sql = "SELECT AVG(cena) as povprecna_cena,znamka FROM Oglas
+            GROUP BY znamka";
+
+    if ($result = mysqli_query($conn,$sql)) {
+        return $result;
+    } else {
+        return false;
+    }       
+}
+
+
+function najdrazjiAvto($conn) {
+    $sql = "SELECT MAX(ogl.cena) as cena,`status`,created_at,model,znamka FROM Oglas ogl";
+
+    if ($result = mysqli_query($conn,$sql)) {
+        return $result;
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    } 
+}
+
+function najcenejsiAvto($conn) {
+    $sql = "SELECT MIN(ogl.cena) as cena,`status`,created_at,model,znamka FROM Oglas ogl";
+
+    if ($result = mysqli_query($conn,$sql)) {
+        return $result;
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
 }
 
 
--- components/admin/statistika.php
+++ components/admin/statistika.php
@@ -11,7 +11,20 @@ require ("../logic/errorCheckerFunctions.php");
        array()
     );
 
+    $modeliArr = array(
+        array()
+    );
+
+    $averageZnamka = array(
+        array()
+    );
+
     $oglasiResult = numberOfZnamkaLastMonth($conn);
+    $modeliResult = numberOfModelLastMonth($conn);
+    $averageResult = averageCenaAvtov($conn);
+    $averageZnamkaResult = averageCenaNaZnamko($conn);
+    $najdrazjiResult = najdrazjiAvto($conn);
+    $najcenejsiResult = najcenejsiAvto($conn);
     $i = 0;
     while ($row = mysqli_fetch_assoc($oglasiResult)) {
         
@@ -23,13 +36,44 @@ require ("../logic/errorCheckerFunctions.php");
         $i++;
         
     }
+    $i = 0;
+    while ($rowModel = mysqli_fetch_assoc($modeliResult)) {
+        $modeliArr[$i]["stProdanih"] = $rowModel['koliko'];
+        $modeliArr[$i]["model"] = $rowModel["model"];
+       /* echo "<pre style='margin-left: 100px;'>";
+        var_dump($rowModel);
+        echo "</pre>"; */
+        $i++;
+    }
+
+    while ($rowAverage = mysqli_fetch_assoc($averageResult)) {
+        $povprecnaCena = $rowAverage['povprecna_cena'];
+    }
+
+    $i = 0;
+    while ($rowAverageZnamka = mysqli_fetch_assoc($averageZnamkaResult)) {
+        $averageZnamka[$i]["znamka"] = $rowAverageZnamka["znamka"];
+        $averageZnamka[$i]["povprecna_cena"] = $rowAverageZnamka["povprecna_cena"];
+
+        $i++;
+    }
+
+    $najdrazjiAvto = mysqli_fetch_all($najdrazjiResult,MYSQLI_ASSOC);
+    $najcenejsiAvto = mysqli_fetch_all($najcenejsiResult,MYSQLI_ASSOC);
+    $najdrazji = $najdrazjiAvto[0];
+    $najcenejsi = $najcenejsiAvto[0];
+
+    echo "<pre style='margin-left: 100px;'>";
+    var_dump($najdrazji);
+    echo "</pre>";
+    
 
 
     
-   /* echo "<pre style='margin-left: 100px;'>";
-    var_dump($oglasiArr);
-    echo count($oglasiArr);
-    echo "</pre>"; */
+    /*echo "<pre style='margin-left: 100px;'>";
+    var_dump($averageZnamka);
+    echo count($averageZnamka); */
+    echo "</pre>"; 
 /*}*/
 
 
@@ -50,16 +94,34 @@ require ("../logic/errorCheckerFunctions.php");
     <?php require ("Sidebar.php") ?>
     <main>
         <div class="main-page-wrapper">
+
+            <div class='stats-wrapper'>
+                <div class='stat'>
+                    <p>Povprečna cena vozil: <?php echo number_format($povprecnaCena,2,',','') . " €" ?></p>
+                    <?php  
+                        for ($i = 0; $i < count($averageZnamka); $i++) {
+                        echo $averageZnamka[$i]['znamka'] .": " . number_format($averageZnamka[$i]['povprecna_cena'],2,',','')  . " €" . "</br>";
+                        }
+                        
+                    ?>
+                </div>
+
+                <div class='stat'>
+                    <h1>MAXIMUM - MINIMUM</h1>
+                    <p>Najdražji avto: <?php echo  number_format($najdrazji['cena']) . " €"; ?></p>
+                    <p>Najcenejši avto: <?php echo  number_format($najcenejsi['cena']) . " €"; ?></p>
+                </div>
+            </div>
+
             <div class='graphs-wrapper'>
+
                 <div class='graph'>
                     <canvas id="myCanvas" width="500" height="500"></canvas>
                 </div>
                 <div class='graph'>
-                    neki
+                    <canvas id="st-modelov-prodanih" width="500" height="500"></canvas>
                 </div>
-                <div class='graph'>
-                    neki
-                </div>
+                
                 <div class='graph'>
                     neki
                 </div>
@@ -74,15 +136,32 @@ require ("../logic/errorCheckerFunctions.php");
 
 let znamkeArr = [];
 let znamkeCountArr = [];
+
+let modeliArr = [];
+let modelCountArr = [];
+
 <?php for($i = 0; $i < count($oglasiArr); $i++) { ?>
 
     znamkeArr.push("<?php echo $oglasiArr[$i]["znamka"]; ?>");
-    znamkeCountArr.push("<?php echo $oglasiArr[$i]["stProdanih"] ?>");
+    znamkeCountArr.push("<?php echo $oglasiArr[$i]["stProdanih"] ;?>");
 <?php } ?>
+
+
+<?php 
+
+        for ($i = 0; $i < count($modeliArr); $i++) {  ?>
+            modeliArr.push("<?php echo $modeliArr[$i]["model"] ?>");
+            modelCountArr.push("<?php echo $modeliArr[$i]["stProdanih"];?>");
+<?php   } ?>
+
+
+
+
 
 console.log(znamkeArr);
 console.log(znamkeCountArr);
-
+console.log(modeliArr);
+console.log(modelCountArr);
 
 let barColors = ["#E29CF3", "#E29CF3","#9740E9","#6764EF","#9B9DA2"];
 
@@ -113,6 +192,37 @@ let myChart = new Chart("myCanvas", {
     },
   },
 });
+
+
+
+let myGraph = new Chart("st-modelov-prodanih", {
+  type: "bar",
+  data: {
+    labels: modeliArr,
+    datasets: [{
+      backgroundColor: barColors,
+      data: modelCountArr
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Število novih prodajanih vozil v zadnjem mesecu"
+    },
+    responsive: false,
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true,
+            },
+        }],
+    },
+  },
+});
+
+
+
 </script>
 </body>
 </html>
